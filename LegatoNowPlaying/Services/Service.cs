39ab1e83f3c2265e33543ad06480b098a32aa119
marihachi <marihachi0620@gmail.com>
2018-06-16 16:34:11 +0900

    refactoring services

---
+++ LegatoNowPlaying/Services/Service.cs
@@ -0,0 +1,18 @@
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace LegatoNowPlaying.Services
+{
+	public abstract class Service
+	{
+		public abstract string Name { get; }
+		public abstract bool IsInstalled { get; }
+		public abstract bool HasSetting { get; }
+		public bool Enabled { get; set; } = true;
+
+		public abstract Task<bool> Install();
+		public abstract Task Setup();
+		public abstract Task Post(string text, Image albumArt);
+		public abstract Task Setting();
+	}
+}
